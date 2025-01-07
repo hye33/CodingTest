4ab7b820a74608a6c961134f0deb14dad8c7e9f5
Hyerin <128382222+hye33@users.noreply.github.com>
2025-01-07 16:47:02 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    queue<int> cards;
+    for (int i = 1; i <= n; i++)
+        cards.push(i);
+    
+    while (cards.size() > 2)
+    {
+        cards.pop();
+        cards.push(cards.front());
+        cards.pop();
+    }
+    if (cards.size() != 1) cards.pop();
+    cout << cards.front();
+    
+}
