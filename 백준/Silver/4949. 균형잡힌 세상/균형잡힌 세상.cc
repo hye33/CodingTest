f2811f49bcc347e3b7a48f4d8be93bdc59e68e8d
Hyerin <128382222+hye33@users.noreply.github.com>
2025-01-10 12:01:24 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int dp[5001];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    while (1)
+    {
+        string str;
+        stack<char> s;
+        getline(cin, str);
+        if (str == ".")
+            break;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            char c = str[i];
+            if (c == '(' || c == '[')
+                s.push(c);
+            else if (c == ')')
+            {
+                if (s.empty())
+                {
+                    s.push(c);
+                    break;
+                }
+                if (s.top() == '(')
+                    s.pop();
+                else
+                {
+                    s.push(c);
+                    break;
+                }
+            }
+            else if (c == ']')
+            {
+                if (s.empty())
+                {
+                    s.push(c);
+                    break;
+                }
+                if (s.top() == '[')
+                    s.pop();
+                else
+                {
+                    s.push(c);
+                    break;
+                }
+            }
+        }
+
+        if (s.empty())
+            cout << "yes" << '\n';
+        else
+            cout << "no" << '\n';
+    }
+}
