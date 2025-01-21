59cd054469bc9402f9a18b7fff42ebc0966240c9
Hyerin <128382222+hye33@users.noreply.github.com>
2025-01-21 20:26:33 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    priority_queue<int> q;
+    int n, input;
+    cin >> n;
+    while (n--)
+    {
+        cin >> input;
+        if (input == 0)
+        {
+            if (q.empty())
+                cout << 0 << '\n';
+            else
+            {
+                cout << q.top() << '\n';
+                q.pop();
+            }
+        }
+        else
+            q.push(input);
+    }
+}
