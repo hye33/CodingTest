e945aed0578686e2a3a1baeace710eb56fe46039
Hyerin <128382222+hye33@users.noreply.github.com>
2025-01-30 17:27:15 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 12 ms, Memory: 2732 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -13,16 +13,13 @@ int main()
     int n, m, k;
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            cin >> arr[i][j];
-
-    ps[1][1] = arr[1][1];
-    for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
+        {
+            cin >> arr[i][j];
             ps[i][j] = ps[i][j - 1] + arr[i][j];
-
-        if (i != n ) ps[i + 1][0] = ps[i][m];
+        }
+        if (i != n) ps[i + 1][0] = ps[i][m];
     }
 
     cin >> k;
@@ -31,10 +28,8 @@ int main()
         int sum = 0;
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        
         for (int i = a; i <= c; i++)
             sum += (ps[i][d] - ps[i][b - 1]);
-
         cout << sum << '\n';
     }
 }
