c624b8d3deac43a256c0939b3dbadc34e6fea8ac
Hyerin <128382222+hye33@users.noreply.github.com>
2025-01-31 18:21:26 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,34 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
-vector<int> vec[101];
+bool g[101][101];
 bool visit[101];
-
-bool bfs(int node, int target)
-{
-    queue<int> q;
-    visit[node] = true;
-    q.push(node);
-
-    while(!q.empty())
-    {
-        int crt = q.front();
-        q.pop();
-        for (int i = 0; i < vec[crt].size(); i++)
-        {
-            int c = vec[crt][i];
-            if (c == target) return true;
-            if (!visit[c])
-            {
-                visit[c] = true;
-                q.push(c);
-            }
-        }
-    }
-    return false;
-}
 
 int main()
 {
@@ -38,22 +13,20 @@ int main()
 
     int n, input;
     cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cin >> input;
-            if (input) vec[i].push_back(j);
-        }
-    }
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> g[i][j];
+
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (g[i][k] && g[k][j]) g[i][j] = true;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
-        {
-            cout << bfs(i, j) << ' ';
-            fill_n(visit, n + 1, false);
-        }
+        for (int j = 0; j < n; j++)
+            cout << g[i][j] << ' ';
         cout << '\n';
     }
 }
