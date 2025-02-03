cd885b516d90c341089c17cee4ac89b71b86bb36
Hyerin <128382222+hye33@users.noreply.github.com>
2025-02-03 23:54:47 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int l;
+    cin >> l;
+    if (l % 5 == 0)
+        cout << l / 5;
+    else
+        cout << l / 5 + 1;
+}
