35874f7f877900549bbf1473ca12ab7db56db43d
Hyerin <128382222+hye33@users.noreply.github.com>
2025-02-06 23:18:06 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int arr[1000001];
+int arr[1000001] = { 0, 0, 1, 1 };
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    arr[1] = 0; arr[2] = 1; arr[3] = 1;
+    
     int n;
     cin >> n; 
     for (int i = 4; i <= n; i++)
     {
-        arr[i] = 1000000;
-        if (i % 3 == 0) arr[i] = arr[i / 3] + 1;
-        if (i % 2 == 0) arr[i] = min(arr[i / 2] + 1, arr[i]);
-        arr[i] = min(arr[i - 1] + 1, arr[i]);
+        arr[i] = arr[i - 1] + 1;
+        if (i % 3 == 0) arr[i] = min(arr[i / 3] + 1, arr[i]);
+        if (i % 2 == 0) arr[i] = min(arr[i / 2] + 1, arr[i]);   
     }
-
     cout << arr[n];
 }
