034baba96c997b19ccab517a9e75762ac4a5db20
Hyerin <128382222+hye33@users.noreply.github.com>
2026-01-12 16:36:21 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, l;
+    cin >> n >> l;
+    for (int i = 0; i < l - 1; i++)
+        cout << 1;
+    cout << n;
+}
